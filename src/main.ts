af7c5f6e83f67f97c5ccc8d0d398a494674e4c35
Miguel Guell <65102622+MiguelGDD@users.noreply.github.com>
2026-07-11 21:18:55 -0500

    chore: add fly.io deployment config

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  app.enableShutdownHooks();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -25,7 +26,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, documentConfig);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 
 void bootstrap();
